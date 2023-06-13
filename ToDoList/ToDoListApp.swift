98e74843c810e387213da8f23a983f0b10c515aa
Jocelyn <jocelyn725paek@gmail.com>
2023-06-13 08:22:35 -0700

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Jocelyn Paek on 6/13/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
